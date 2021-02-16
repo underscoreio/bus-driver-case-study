a369b6d37963034b4d180bee612d5ecc8215690d
Sophie Still <35223450+sophie-still@users.noreply.github.com>
2021-02-16 10:57:20 +0000

    solution

--- src/main/scala/code/GossipFinder.scala
+++ src/main/scala/code/GossipFinder.scala
@@ -2,24 +2,44 @@ package code
 
 case class Driver(id: Int, stops: List[Int], gossipReceivedFrom: Set[Int]) {
 
-  def hasReceivedGossipFromAll(numberOfDrivers: Int) = gossipReceivedFrom.size == numberOfDrivers - 1
+  def hasReceivedGossipFromAll(numberOfDrivers: Int) = gossipReceivedFrom.size == numberOfDrivers
 
   def getStopForCurrentIteration(index: Int) : Int = {
     stops(index % stops.length)
   }
 
-  def withGossipFrom(driver: Int):Driver = {
-    this.copy(gossipReceivedFrom = this.gossipReceivedFrom + driver)
+  def withGossipFrom(drivers: Set[Int]):Driver = {
+    this.copy(gossipReceivedFrom = this.gossipReceivedFrom ++ drivers)
   }
 
 }
 
 object GossipFinder extends App {
 
-  def findGossip(drivers: Seq[Driver]): Option[Int]
-  =
-  {
-//    val numberOfDrivers = drivers.size(1 to 480).foreach(index => {}) None
-    ???
+  def findGossip(min: Int = 0, drivers: Seq[Driver]): Option[Int] = {
+    println(s" min=> $min   driver = $drivers")
+    println("======")
+    if (min >= 480) None
+    else {
+      if (drivers.forall(_.hasReceivedGossipFromAll(drivers.size)))
+        Some(min)
+      else {
+        findGossip(min + 1, exchangeGossips(min, drivers))
+      }
+    }
+  }
+
+  def exchangeGossips(min: Int, drivers: Seq[Driver]): Seq[Driver] = {
+    drivers.map(driver => exchangeGossip(min, driver, drivers ))
+  }
+  def exchangeGossip(min: Int, driver: Driver, drivers: Seq[Driver]): Driver = {
+    // find stop of driver
+    val stop = driver.getStopForCurrentIteration(min)
+
+    //iterate drivers excluding driver => compare whether they are at same stop
+    val newGossipReceived =
+      drivers.filter(_.getStopForCurrentIteration(min) == stop)
+        .flatMap(_.gossipReceivedFrom).toSet
+    driver.withGossipFrom(newGossipReceived)
   }
 }
--- src/test/scala/code/GossipFinderSpec.scala
+++ src/test/scala/code/GossipFinderSpec.scala
@@ -7,11 +7,11 @@ class GossipFinderSpec extends WordSpec with Matchers {
     "example1 - three drivers exchanging all gossip after 5 stops" in {
 
       val drivers = Seq(
-        Driver(1, List(3, 1, 2, 3), Set()),
-        Driver(2, List(3, 2, 3, 1), Set()),
-        Driver(3, List(4, 2, 3, 4, 5), Set())
+        Driver(1, List(3, 1, 2, 3), Set(1)),
+        Driver(2, List(3, 2, 3, 1), Set(2)),
+        Driver(3, List(4, 2, 3, 4, 5), Set(3))
       )
-      GossipFinder.findGossip(drivers) shouldBe Some(5)
+      GossipFinder.findGossip(0,drivers) shouldBe Some(5)
     }
   }
 
@@ -34,7 +34,20 @@ class GossipFinderSpec extends WordSpec with Matchers {
   "driver.withGossip" should {
     "give us a new Driver who has heard gossip from another driver" in {
       val expectedDriver = Driver(1, List(3, 1, 2, 4), Set(2, 3, 4))
-      Driver(1, List(3, 1, 2, 4), Set(2, 3)).withGossipFrom(4) shouldBe expectedDriver
+      Driver(1, List(3, 1, 2, 4), Set(2, 3)).withGossipFrom(Set(4)) shouldBe expectedDriver
     }
   }
+
+  //  "driver.exchangeGossip" should {
+  //    "exchange gossip with another driver if they are same stop" in {
+  //      val driver1 = Driver(1, List(3, 1, 2, 4), Set())
+  //      val driver2 = Driver(2, List(3, 2, 3, 1), Set())
+  //
+  //      driver1.exchangeGossip(driver2)
+  //
+  //      driver1 shouldBe Driver(1, List(3, 1, 2, 4), Set(2))
+  //      driver2 shouldBe Driver(2, List(3, 2, 3, 1), Set(1))
+  //
+  //    }
+  //  }
 }
